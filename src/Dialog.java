b3f86ffb49572a44f938b2a76ae2e2266fdc92ff
Forrie Shinn <ForrieShinn@gmail.com>
2018-10-16 09:15:37 -0700

    added GUI components to settings dialog (no functionality yet)

--- src/Dialog.java
+++ src/Dialog.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.border.*;
 
 /* Dialog Class
  * No description yet
@@ -8,6 +9,7 @@ import java.awt.event.*;
 
 class Dialog extends JDialog {
     JPanel inputPanel, confirmPanel;
+    JTextField height,width,mines;
     
     public Dialog (Window owner) {
         // create dialog
@@ -28,6 +30,18 @@ class Dialog extends JDialog {
         // create panel
         inputPanel = new JPanel();
         inputPanel.setBackground(Color.LIGHT_GRAY);
+        // pads the panel
+        inputPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        // params: rows, cols, hgap, vgap
+        inputPanel.setLayout(new GridLayout(3, 2, 25, 7));
+        
+        // add content
+        inputPanel.add(new JLabel("Height"));
+        inputPanel.add((height = new JTextField()));
+        inputPanel.add(new JLabel("Width"));
+        inputPanel.add((width = new JTextField()));
+        inputPanel.add(new JLabel("Mines"));
+        inputPanel.add((mines = new JTextField()));
         
         return inputPanel;
     }
@@ -35,7 +49,31 @@ class Dialog extends JDialog {
     private JPanel createConfirmPanel() {
         // create panel
         confirmPanel = new JPanel();
-        confirmPanel.setBackground(Color.GRAY);
+        confirmPanel.setBackground(Color.LIGHT_GRAY);
+        
+        // create and add confirmbutton
+        JButton confirmbutton = new JButton("OK");
+        // add reset functionality to button
+        confirmbutton.addActionListener(
+            new AbstractAction("newGame") {
+                public void actionPerformed(ActionEvent e) {
+                    Minesweeper.newBoard();
+                }
+            }
+        );
+        confirmPanel.add(confirmbutton);
+        
+        // create and add cancelbutton
+        JButton cancelbutton = new JButton("Cancel");
+        // add reset functionality to button
+        cancelbutton.addActionListener(
+            new AbstractAction("Cancel") {
+                public void actionPerformed(ActionEvent e) {
+                    Minesweeper.newBoard();
+                }
+            }
+        );
+        confirmPanel.add(cancelbutton);
         
         return confirmPanel;
     }
